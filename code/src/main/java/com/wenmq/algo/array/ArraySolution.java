3694ae92a5a8d7f313691cd5b7b351be5b69cbd4
ifans.wen <ifans.wen@corp.to8to.com>
2020-07-07 19:02:23 +0800

    [ADD] 2020-7-7 15:17:01 add binary solution

--- code/src/main/java/com/wenmq/algo/array/ArraySolution.java
+++ code/src/main/java/com/wenmq/algo/array/ArraySolution.java
@@ -8,10 +8,14 @@ package com.wenmq.algo.array;
 public class ArraySolution {
 
     public static void main(String[] args) {
+        int[][] aa = new int[][]{{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}};
+        int[][] bb = new int[][]{{2, 2, 2}, {2, 1, 2}, {2, 2, 2}};
+        int[][] cc = new int[][]{{1, 1}, {3, 4}, {-1, 0}};
+        System.out.println(minTimeToVisitAllPoints(cc));
+//        System.out.println(surfaceArea(bb));
 //        System.out.println(numWays(7));
 //        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
-        System.out.println(hasGroupsSizeX(new int[]{1, 2, 3, 4, 4, 3, 2, 1}));
-//        int[][] aa = new int[][]{{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}};
+//        System.out.println(hasGroupsSizeX(new int[]{1, 2, 3, 4, 4, 3, 2, 1}));
 //
 //        int[][] bb = new int[10][10];
 //        int i = 0;
@@ -112,5 +116,52 @@ public class ArraySolution {
 
     static private int gcd(int a, int b) {
         return b == 0 ? a : gcd(b, a % b);
+    }
+
+    //  容斥原理
+    static public int surfaceArea(int[][] grid) {
+        int n = grid.length;
+        int area = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // 容斥原理的核心
+                // 先忽略所有的重叠和重复计算
+                int num = grid[i][j];
+                area += num * 6;
+                // 计算完统计后再执行减操作
+                // 竖直方向重叠面的个数
+                area -= Math.max((num - 1) << 1, 0);
+                // 与相邻左或上重叠面的个数
+                area -= getConflictLT(grid, i, j);
+            }
+        }
+        return area;
+    }
+
+    /**
+     * 计算指定节点与相邻节点的冲突
+     * 容易越界，所以单独抽出来
+     */
+    static public int getConflictLT(int[][] grid, int i, int j) {
+        int left = 0;
+        int top = 0;
+        int num = grid[i][j];
+        if (i != 0) {
+            top = Math.min(grid[i - 1][j], num);
+        }
+        if (j != 0) {
+            left = Math.min(grid[i][j - 1], num);
+        }
+        return (left + top) * 2;
+    }
+
+
+    public static int minTimeToVisitAllPoints(int[][] points) {
+        if (points.length < 2 || points[0].length < 2) return 0;
+        int sum = 0;
+        for (int i = 1; i < points.length; i++) {
+            sum += Math.max(Math.abs(points[i][0] - points[i-1][0]), Math.abs(points[i][1] - points[i - 1][1]));
+        }
+        return sum;
     }
 }
