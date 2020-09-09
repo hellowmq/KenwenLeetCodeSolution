689e137519d0646be377be843926584af805c99b
ifans.wen <ifans.wen@corp.to8to.com>
2020-09-09 11:15:32 +0800

    [ADD]  2020-09-09 11:15:22 add doc

--- code/src/main/java/com/wenmq/byt/ExceptionTest.java
+++ code/src/main/java/com/wenmq/byt/ExceptionTest.java
@@ -1,11 +1,20 @@
 package com.wenmq.byt;
 
 /**
- * Created by  @ifans.wen
+ * 当发生 return 时，
+ * ``编译器`` 插入了一个 临时变量用于缓存 return 的操作数（字面量或者引用）
+ * 除了通过内存地址的修改，否则一切对于返回操作数的修改都将不起作用
+ * 无效的操作包括：
+ * - 修改字面量的值
+ * - 修改引用的地址
  *
- * @date 2020/9/9.
+ *
+ * jvm 指令中，finally 语法块会以冗余的方式插入到 try 和 catch 之后
+ *
  */
 public class ExceptionTest {
+
+
 //    public String testStringException() {
 //
 //        try {
@@ -21,6 +30,10 @@ public class ExceptionTest {
 //        }
 //    }
 
+    /**
+     * @return 1111111 when exception
+     *         1111111 when no exception
+     */
     public String testException2() {
         String s = "";
         try {
@@ -37,6 +50,11 @@ public class ExceptionTest {
         }
     }
 
+    /**
+     * @return 2222222 when exception
+     *         2222222 when no exception
+     * return 语句会更新到局部变量表
+     */
     public String testException3() {
         String s;
         try {
@@ -54,6 +72,10 @@ public class ExceptionTest {
         }
     }
 
+    /**
+     * @return 1 when exception
+     *         0 when no exception
+     */
     public int testException4() {
         int s;
         try {
@@ -69,25 +91,30 @@ public class ExceptionTest {
             s = 2;
         }
     }
-
-    public int testException5() {
+    /**
+     * @return [1] when exception
+     *         [2] when no exception
+     */
+    public int[] testException5() {
         int[] s = {0};
         try {
             inside_try();
             s[0] = 0;
-            System.out.println(s[0]);
-            return s[0];
+            return s;
         } catch (Exception e) {
             inside_catch(e);
             s[0] = 1;
-            System.out.println(s[0]);
-            return s[0];
+            return s;
         } finally {
             inside_finally();
             s[0] = 2;
         }
     }
 
+    /**
+     * @return 11111112222222 when exception
+     *         00000002222222 when no exception
+     */
     public StringBuilder testException6() {
         StringBuilder s = new StringBuilder();
         try {
--- code/src/main/java/com/wenmq/byt/TestMain.java
+++ code/src/main/java/com/wenmq/byt/TestMain.java
@@ -17,14 +17,14 @@ public class TestMain {
         System.out.println(exceptionTest.testException2());
         System.out.println(exceptionTest.testException3());
         System.out.println(exceptionTest.testException4());
-        System.out.println(exceptionTest.testException5());
+        System.out.println(exceptionTest.testException5()[0]);
         System.out.println(exceptionTest.testException6());
         System.out.println("---------------");
 //        System.out.println(exceptionTest2.testStringException());
         System.out.println(exceptionTest2.testException2());
         System.out.println(exceptionTest2.testException3());
         System.out.println(exceptionTest2.testException4());
-        System.out.println(exceptionTest2.testException5());
+        System.out.println(exceptionTest2.testException5()[0]);
         System.out.println(exceptionTest2.testException6());
     }
 }
