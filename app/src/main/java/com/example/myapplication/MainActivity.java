204d2985890db178f4b4d6aeb91a86f91ebde2d2
hellowmq <wenmq47@gmail.com>
2020-09-21 01:17:38 +0800

    [ADD] 2020-09-21 01:17:28 add okhttp

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,17 +16,24 @@ import com.example.myapplication.bean.BaseSelectData;
 import com.example.myapplication.bean.CommonTitleBean;
 import com.example.myapplication.bean.DataType;
 import com.example.myapplication.urtils.SelectUtils;
-import com.wenmq.anno.GetMsg;
-import com.wenmq.clickanno.OnceClick;
-import com.wenmq.clickapi.OnceInit;
+//import com.wenmq.anno.GetMsg;
+//import com.wenmq.clickanno.OnceClick;
+//import com.wenmq.clickapi.OnceInit;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-@GetMsg(id = 2, name = "MAIN")
+
+//@GetMsg(id = 2, name = "MAIN")
 public class MainActivity extends AppCompatActivity {
+
 
 
     private ListView mListView;
@@ -62,44 +70,85 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void clickByPosition(final int position) {
+        requestByUrl("https:www.baidu.com");
 
-        if (position < 10) {
-            SelectUtils.showTimeUltimateSelect(MainActivity.this
-                    , new SelectUtils.TimeSelectCallback() {
-                        @Override
-                        public void onSelectData(Date date) {
-                            Toast.makeText(MainActivity.this, (date).toString(), Toast.LENGTH_SHORT).show();
-                            mList.get(position).setTitle(date.toString());
-                            if (mButtonItemAdapter != null) {
-                                mButtonItemAdapter.notifyDataSetChanged();
-                            }
-                        }
-                    });
-        } else if (position < 20) {
-            SelectUtils.showCommonSelect(MainActivity.this, new SelectUtils.CommonMultiPickCallback() {
-                @Override
-                public void onSelectData(List<BaseSelectData> selectData) {
-                    String tx = selectData.get(0).getPickerViewText()
-                            + selectData.get(1).getPickerViewText();
-//                        + options3Items.get(options1).get(option2).get(options3).getPickerViewText();
-                }
-            });
 
-        } else {
-            SelectUtils.showTimeSelect(MainActivity.this
-                    , new SelectUtils.TimeSelectCallback() {
-                        @Override
-                        public void onSelectData(Date date) {
-                            Toast.makeText(MainActivity.this, (date).toString(), Toast.LENGTH_SHORT).show();
-                            mList.get(position).setTitle(date.toString());
-                            if (mButtonItemAdapter != null) {
-                                mButtonItemAdapter.notifyDataSetChanged();
-                            }
-                        }
-                    });
-        }
+//        if (position < 10) {
+//            SelectUtils.showTimeUltimateSelect(MainActivity.this
+//                    , new SelectUtils.TimeSelectCallback() {
+//                        @Override
+//                        public void onSelectData(Date date) {
+//                            Toast.makeText(MainActivity.this, (date).toString(), Toast.LENGTH_SHORT).show();
+//                            mList.get(position).setTitle(date.toString());
+//                            if (mButtonItemAdapter != null) {
+//                                mButtonItemAdapter.notifyDataSetChanged();
+//                            }
+//                        }
+//                    });
+//        } else if (position < 20) {
+//            SelectUtils.showCommonSelect(MainActivity.this, new SelectUtils.CommonMultiPickCallback() {
+//                @Override
+//                public void onSelectData(List<BaseSelectData> selectData) {
+//                    String tx = selectData.get(0).getPickerViewText()
+//                            + selectData.get(1).getPickerViewText();
+////                        + options3Items.get(options1).get(option2).get(options3).getPickerViewText();
+//                }
+//            });
+//
+//        } else {
+//            SelectUtils.showTimeSelect(MainActivity.this
+//                    , new SelectUtils.TimeSelectCallback() {
+//                        @Override
+//                        public void onSelectData(Date date) {
+//                            Toast.makeText(MainActivity.this, (date).toString(), Toast.LENGTH_SHORT).show();
+//                            mList.get(position).setTitle(date.toString());
+//                            if (mButtonItemAdapter != null) {
+//                                mButtonItemAdapter.notifyDataSetChanged();
+//                            }
+//                        }
+//                    });
+//        }
 
-        OnceInit.once(this, 2000);
+//        OnceInit.once(this, 2000);
+    }
+
+    private void requestByUrl(String url) {
+        OkHttpClient client = new OkHttpClient();
+
+        Log.e(MainActivity.class.getSimpleName(), "start");
+        Request request = new Request.Builder()
+                .get()
+                .url(url)
+                .build();
+        Call call = client.newCall(request);
+        //异步调用,并设置回调函数
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                Log.e(MainActivity.class.getSimpleName(), "onFailed" );
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "get failed", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+
+                Log.e(MainActivity.class.getSimpleName(), "succes");
+                final String res = response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.e(MainActivity.class.getSimpleName(), res);
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -132,7 +181,7 @@ public class MainActivity extends AppCompatActivity {
 //    }
 
 
-    @OnceClick(value = R.id.fab)
+//    @OnceClick(value = R.id.fab)
     private void selectFab() {
         SelectUtils.showCommonSelect(MainActivity.this, new SelectUtils.CommonMultiPickCallback() {
             @Override
