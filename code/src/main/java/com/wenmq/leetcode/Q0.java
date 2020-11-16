12fa0c01b4d9c7fea8e65cbc66891df2d55b3bde
hellowmq <wenmq47@gmail.com>
2020-11-16 14:32:13 +0800

    [ADD] 2020-11-16 10:21:09 add Solution Q389 & template

---
+++ code/src/main/java/com/wenmq/leetcode/Q0.java
@@ -0,0 +1,19 @@
+package com.wenmq.leetcode;
+
+import com.wenmq.algo.base.ListNode;
+import com.wenmq.tools.ListNodeTools;
+
+/**
+ * Q148
+ * https://leetcode-cn.com/problems/find-the-difference/
+ */
+public class Q0 {
+    static class Solution {
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+
+}
---
+++ code/src/main/java/com/wenmq/leetcode/Q389.java
@@ -0,0 +1,29 @@
+package com.wenmq.leetcode;
+
+/**
+ * Q389
+ * https://leetcode-cn.com/problems/find-the-difference/
+ */
+public class Q389 {
+    static class Solution {
+        /**
+         * 利用异或操作的性质可以有 a ^ a = 0
+         */
+        public char findTheDifference(String s, String t) {
+            char[] ss = s.toCharArray();
+            char[] tt = t.toCharArray();
+            char a = (char) 0;
+            for (char b : ss) a ^= b;
+            for (char b : tt) a ^= b;
+            return a;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "abcd";
+        String t = "abcde";
+        System.out.println(solution.findTheDifference(s, t));
+
+    }
+}
