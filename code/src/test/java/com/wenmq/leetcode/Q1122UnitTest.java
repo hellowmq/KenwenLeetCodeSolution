fa2bdefd0861878f7fc0080f931511fa050d84ac
hellowmq <wenmq47@gmail.com>
2020-11-26 23:57:56 +0800

    [FEATURE] 2020-11-26 23:57:44 add unit_test template

--- code/src/test/java/com/wenmq/leetcode/Q1122UnitTest.java
+++ code/src/test/java/com/wenmq/leetcode/Q1122UnitTest.java
@@ -5,12 +5,14 @@ import org.junit.Test;
 
 import static com.google.common.truth.Truth.assertThat;
 
-public class Q1122Test {
+public class Q1122UnitTest {
     @Test
     public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
         Q1122.Solution sl = new Q1122.Solution();
         int[] a = {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19};
         int[] b = {2, 1, 4, 3, 9, 6};
-//        assertThat(sl.relativeSortArray(a, b).equals(int[]{2,2,2,1,4,3,3,9,6,7,19}));
+        int[] assertResult = new int[]{2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19};
+        int[] sortArray = sl.relativeSortArray(a, b);
+        assertThat(sortArray).isEqualTo(assertResult);
     }
 }
