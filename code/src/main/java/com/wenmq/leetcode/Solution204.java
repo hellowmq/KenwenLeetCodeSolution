c8e56480641c35381ff58f5f20db39dbe3af463a
hellowmq <wenmq47@gmail.com>
2020-12-03 01:31:54 +0800

    [ADD] 2020-12-03 01:31:48 leetcode 204 & test

--- code/src/main/java/com/wenmq/leetcode/Solution204.java
+++ code/src/main/java/com/wenmq/leetcode/Solution204.java
@@ -14,16 +14,37 @@ public class Solution204 {
         boolean[] b = new boolean[n];
         if (2 < n) result++;
         for (int i = 3; i < n; i += 2) {
-            if (!b[i]) {
-                if (i <= sqrtN) {
-                    for (int j = i; i * j < n; j += 2) {
-                        b[i * j] = true;    // 将当前质数的奇数倍都设置成非质数标记 true
-                    }
+            if (b[i]) continue;
+            if (i <= sqrtN) {
+                int temp = i * i;
+                for (int j = i; temp < n; j += 2, temp = i * j) {
+                    b[temp] = true;
                 }
-                result++;   // 质数个数 +1
             }
+            result++;   // 质数个数 +1
         }
         return result;
+    }
+
+
+    public int countPrimes2(int n) {
+        if (n < 3) {
+            return 0;
+        }
+        boolean[] f = new boolean[n];
+        int count = n / 2;
+        for (int i = 3; i * i < n; i += 2) {
+            if (f[i]) {
+                continue;
+            }
+            for (int j = i * i; j < n; j += 2 * i) {
+                if (!f[j]) {
+                    --count;
+                    f[j] = true;
+                }
+            }
+        }
+        return count;
     }
 }
 
