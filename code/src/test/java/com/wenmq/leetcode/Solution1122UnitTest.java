aff54470f6df09ceb31c00032b1ec21e77e3782d
hellowmq <wenmq47@gmail.com>
2020-12-05 19:22:57 +0800

    [ADD] 2020-12-05 19:22:47 leetcode 1122 & test

--- code/src/test/java/com/wenmq/leetcode/Solution1122UnitTest.java
+++ code/src/test/java/com/wenmq/leetcode/Solution1122UnitTest.java
@@ -1,18 +1,26 @@
 package com.wenmq.leetcode;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static com.google.common.truth.Truth.assertThat;
 
-public class Q1122UnitTest {
+public class Solution1122UnitTest {
+    String TAG = "com.wenmq.leetcode.Solution1122";
+    Solution1122 mTestSolution;
+
+    @Before
+    public void setUp() throws Exception {
+        mTestSolution = new Solution1122();
+    }
+
     @Test
     public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
-        Q1122.Solution sl = new Q1122.Solution();
         int[] a = {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19};
         int[] b = {2, 1, 4, 3, 9, 6};
         int[] assertResult = new int[]{2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19};
-        int[] sortArray = sl.relativeSortArray(a, b);
+        int[] sortArray = mTestSolution.relativeSortArray(a, b);
         assertThat(sortArray).isEqualTo(assertResult);
     }
 }
