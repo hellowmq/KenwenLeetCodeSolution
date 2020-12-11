6e7820a1167029cfcc52d958ccd27437f70bb3f8
hellowmq <wenmq47@gmail.com>
2020-12-11 22:57:01 +0800

    [ADD] 2020-12-11 22:56:52 add leetcode_648 solution and test

---
+++ code/src/main/java/com/wenmq/leetcode/Solution649.java
@@ -0,0 +1,152 @@
+package com.wenmq.leetcode;
+
+import java.util.LinkedList;
+
+/**
+ * Q649
+ * https://leetcode-cn.com/problems/dota2-senate/
+ */
+public class Solution649 {
+    /**
+     * 方法一：
+     */
+    public String predictPartyVictory(String senate) {
+
+        LinkedList<Boolean> list = new LinkedList<>();
+        char[] chars = senate.toCharArray();
+        int rCount = 0;
+        for (char aChar : chars) {
+            boolean e = aChar == 'R';
+            list.offer(e);
+            if (e) {
+                rCount++;
+            }
+        }
+        int bias = 0;
+        int dCount = chars.length - rCount;
+        while ((((rCount + bias) > 0) &&
+                ((dCount - bias) > 0))) {
+            Boolean poll = list.pop();
+            if (((bias < 0 && poll) ||
+                    (bias > 0 && !poll))) {
+                bias += poll ? 1 : -1;
+                if (poll) {
+                    rCount--;
+
+                } else {
+                    dCount--;
+                }
+                continue;
+            }
+            list.offer(poll);
+            bias += poll ? 1 : -1;
+        }
+        return list.getLast() ? "Radiant" : "Dire";
+    }
+
+    /**
+     * 方法二：
+     */
+    public String predictPartyVictory2(String senate) {
+
+        char[] chars = senate.toCharArray();
+        int rCount = 0;
+        for (char aChar : chars) {
+            if (aChar == 'R') {
+                rCount++;
+            }
+        }
+        int slow = 0;
+        int fast = chars.length;
+        int bias = 0;
+        int dCount = chars.length - rCount;
+        while (!(rCount + bias <= 0 || (dCount - bias) <= 0)) {
+            char pol = chars[slow % chars.length];
+            boolean poll = pol == 'R';
+            if (((bias < 0 && poll) || (bias > 0 && !poll))) {
+                bias += poll ? 1 : -1;
+                if (poll) {
+                    rCount--;
+                } else {
+                    dCount--;
+                }
+                slow++;
+            } else {
+                chars[fast % chars.length] = pol;
+                bias += poll ? 1 : -1;
+                slow++;
+                fast++;
+            }
+        }
+        return chars[(fast - 1) % chars.length] == 'R' ? "Radiant" : "Dire";
+
+    }
+
+    public String predictPartyVictory3(String senate) {
+        char[] chars = senate.toCharArray();
+        boolean[] booleans = new boolean[chars.length << 1];
+
+        int rCount = 0;
+        for (int i = 0; i < chars.length; i++) {
+            booleans[i] = chars[i] == 'R';
+            if (chars[i] == 'R') {
+                rCount++;
+            }
+        }
+        int slow = 0;
+        int fast = chars.length;
+        int bias = 0;
+        int dCount = chars.length - rCount;
+        for (int i = 0; i < booleans.length; i++) {
+            if (rCount + bias <= 0 || (dCount - bias) <= 0) {
+                break;
+            }
+            boolean poll = booleans[slow];
+            if (((bias < 0 && poll) || (bias > 0 && !poll))) {
+                bias += poll ? 1 : -1;
+                if (poll) {
+                    rCount--;
+                } else {
+                    dCount--;
+                }
+                slow++;
+                continue;
+            }
+            booleans[fast] = poll;
+            bias += poll ? 1 : -1;
+            slow++;
+            fast++;
+
+
+        }
+        return booleans[fast - 1] ? "Radiant" : "Dire";
+    }
+
+
+    /**
+     * 方法4：
+     */
+    public String predictPartyVictory4(String senate) {
+
+        char[] chars = senate.toCharArray();
+
+        int slow = 0;
+        int fast = chars.length;
+        int bias = 0;
+        while (fast - slow > Math.abs(bias)) {
+            char pol = chars[slow % chars.length];
+            boolean poll = pol == 'R';
+            if (((bias < 0 && poll) || (bias > 0 && !poll))) {
+                bias += poll ? 1 : -1;
+                slow++;
+            } else {
+                chars[fast % chars.length] = pol;
+                bias += poll ? 1 : -1;
+                slow++;
+                fast++;
+            }
+        }
+        return bias > 0 ? "Radiant" : "Dire";
+
+    }
+}
---
+++ code/src/test/java/com/wenmq/leetcode/Solution649UnitTest.java
@@ -0,0 +1,47 @@
+package com.wenmq.leetcode;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Solution649UnitTest {
+
+    String TAG = "com.wenmq.leetcode.Solution649";
+    private Solution649 mTestSolution;
+
+
+    @Before
+    public void setUp() throws Exception {
+        mTestSolution = new Solution649();
+    }
+
+    @Test
+    public void testPredictPartyVictory_case1() {
+        String senate = "RD";
+        String result = "Radiant";
+        Assert.assertEquals(result, mTestSolution.predictPartyVictory(senate));
+        Assert.assertEquals(result, mTestSolution.predictPartyVictory2(senate));
+        Assert.assertEquals(result, mTestSolution.predictPartyVictory3(senate));
+        Assert.assertEquals(result, mTestSolution.predictPartyVictory4(senate));
+    }
+
+    @Test
+    public void testPredictPartyVictory_case2() {
+        String senate = "RDD";
+        String result = "Dire";
+        Assert.assertEquals(result, mTestSolution.predictPartyVictory(senate));
+        Assert.assertEquals(result, mTestSolution.predictPartyVictory2(senate));
+        Assert.assertEquals(result, mTestSolution.predictPartyVictory3(senate));
+        Assert.assertEquals(result, mTestSolution.predictPartyVictory4(senate));
+    }
+
+    @Test
+    public void testPredictPartyVictory_case3() {
+        String senate = "RRDDD";
+        String result = "Radiant";
+        Assert.assertEquals(result, mTestSolution.predictPartyVictory(senate));
+        Assert.assertEquals(result, mTestSolution.predictPartyVictory2(senate));
+        Assert.assertEquals(result, mTestSolution.predictPartyVictory3(senate));
+        Assert.assertEquals(result, mTestSolution.predictPartyVictory4(senate));
+    }
+}
