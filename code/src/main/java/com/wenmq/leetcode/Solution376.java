9db934e8d7954c023d69393e8b1feba317bc715a
hellowmq <wenmq47@gmail.com>
2020-12-12 22:59:45 +0800

    [ADD] 2020-12-12 22:59:37 add leetcode_376 solution and test

---
+++ code/src/main/java/com/wenmq/leetcode/Solution376.java
@@ -0,0 +1,30 @@
+package com.wenmq.leetcode;
+
+/**
+ * Q376
+ * https://leetcode-cn.com/problems/wiggle-subsequence/
+ */
+public class Solution376 {
+    /**
+     * 方法一：
+     */
+    public int wiggleMaxLength(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return n;
+        }
+        int up = 1;
+        int down = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up = down + 1;
+            }
+            if (nums[i] < nums[i - 1]) {
+                down = up + 1;
+            }
+        }
+        return Math.max(up, down);
+    }
+
+
+}
