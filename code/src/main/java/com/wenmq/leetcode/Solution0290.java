c621f02a3fe2ff987af5860588d8f1c45e2830ea
hellowmq <wenmq47@gmail.com>
2020-12-16 13:51:48 +0800

    [ADD] 2020-12-16 13:51:42 add leetcode_290 solution and test

---
+++ code/src/main/java/com/wenmq/leetcode/Solution0290.java
@@ -0,0 +1,37 @@
+package com.wenmq.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Q290
+ * https://leetcode-cn.com/problems/word-pattern/
+ */
+public class Solution0290 {
+    public boolean wordPattern(String pattern, String s) {
+        String[] map = new String[26];
+        Set<String> set = new HashSet<>();
+        int i = 0;
+        int j = 0;
+        for (; i < pattern.length(); i++, j++) {
+            int c = pattern.charAt(i) - 'a';
+            int k = j;
+            while (j < s.length() && s.charAt(j) != ' ') {
+                ++j;
+            }
+            if (j == k) {
+                return false;
+            }
+            String w = s.substring(k, j);
+            if (map[c] == null && !set.contains(w)) {
+                set.add(w);
+                map[c] = w;
+                continue;
+            } else if (map[c] != null && map[c].equals(w)) {
+                continue;
+            }
+            return false;
+        }
+        return i == pattern.length() && j >= s.length();
+    }
+}
