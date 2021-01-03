70a880411be06d1488be44f73be3f85d51acfe30
hellowmq <wenmq47@gmail.com>
2021-01-03 18:53:49 +0800

    [ADD] 2021-01-03 18:53:12 add solution 86 and test and tool method

---
+++ code/src/main/java/com/wenmq/leetcode/Solution86.java
@@ -0,0 +1,45 @@
+package com.wenmq.leetcode;
+
+import com.wenmq.algo.base.ListNode;
+import com.wenmq.tools.ListNodeTools;
+
+/**
+ * Q86
+ * https://leetcode-cn.com/problems/partition-list/
+ */
+public class Solution86 {
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode lessDump = new ListNode(0);
+        lessDump.next = null;
+        ListNode lessThan = lessDump;
+        ListNode largerDump = new ListNode(0);
+        largerDump.next = null;
+        ListNode largerThan = largerDump;
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur.val >= x) {
+                largerThan.next = cur;
+                cur = cur.next;
+                largerThan = largerThan.next;
+            } else {
+                lessThan.next = cur;
+                cur = cur.next;
+                lessThan = lessThan.next;
+            }
+        }
+        lessThan.next = largerDump.next;
+        largerThan.next = null;
+        return lessDump.next;
+    }
+
+}
