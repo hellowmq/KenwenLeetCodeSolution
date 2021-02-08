2b65f6d7b031d5713ad894e0677968fbb0d506b5
hellowmq <wenmq47@gmail.com>
2021-02-08 19:25:49 +0800

    [ADD] 2021-02-08 19:22:14 add solution 978 and test

---
+++ code/src/test/java/com/wenmq/leetcode/Solution978UnitTest.java
@@ -0,0 +1,43 @@
+package com.wenmq.leetcode;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Solution978UnitTest {
+
+    String TAG = "com.wenmq.leetcode.Solution978";
+    private Solution978 mTestSolution;
+
+    @Before
+    public void setUp() throws Exception {
+        mTestSolution = new Solution978();
+    }
+
+
+    @Test
+    public void testMaxTurbulenceSize_Case1() {
+        int[] array = new int[]{9, 4, 2, 10, 7, 8, 8, 1, 9};
+        int expect = 5;
+        int actual = mTestSolution.maxTurbulenceSize(array);
+        Assert.assertEquals(expect, actual);
+    }
+
+    @Test
+    public void testMaxTurbulenceSize_Case2() {
+        int[] array = new int[]{4,8,12,16};
+        int expect = 2;
+        int actual = mTestSolution.maxTurbulenceSize(array);
+        Assert.assertEquals(expect, actual);
+    }
+
+    @Test
+    public void testMaxTurbulenceSize_Case3() {
+        int[] array = new int[]{100};
+        int expect = 1;
+        int actual = mTestSolution.maxTurbulenceSize(array);
+        Assert.assertEquals(expect, actual);
+    }
+}
