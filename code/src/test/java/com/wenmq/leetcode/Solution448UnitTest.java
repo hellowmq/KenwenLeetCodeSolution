69b62148b0853d1dfe8e2c92522947863730fc7f
hellowmq <wenmq47@gmail.com>
2021-02-13 01:01:05 +0800

    [ADD] 2021-02-13 01:00:38 add leetcode Solution448 & test

---
+++ code/src/test/java/com/wenmq/leetcode/Solution448UnitTest.java
@@ -0,0 +1,36 @@
+package com.wenmq.leetcode;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution448UnitTest {
+
+    private static final String TAG = "com.wenmq.leetcode.Solution448";
+    private Solution448 mTestSolution;
+
+
+    @Before
+    public void setUp() throws Exception {
+        mTestSolution = new Solution448();
+    }
+
+    @Test
+    public void testFindDisappearedNumbers_Case1() {
+        int[] array = new int[]{4, 3, 2, 7, 8, 2, 3, 1};
+        List<Integer> result = Arrays.asList(5, 6);
+        List<Integer> actual = mTestSolution.findDisappearedNumbers(array);
+        Assert.assertEquals(result.toString(), actual.toString());
+    }
+
+    @Test
+    public void testFindDisappearedNumbers2_Case1() {
+        int[] array = new int[]{4, 3, 2, 7, 8, 2, 3, 1};
+        List<Integer> result = Arrays.asList(5, 6);
+        List<Integer> actual = mTestSolution.findDisappearedNumbers2(array);
+        Assert.assertEquals(result.toString(), actual.toString());
+    }
+}
