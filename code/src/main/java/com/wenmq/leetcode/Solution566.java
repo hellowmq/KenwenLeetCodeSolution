22fa586dee3d36782ff8e9cdfa4823ba2b774b4e
hellowmq <wenmq47@gmail.com>
2021-02-17 01:32:07 +0800

    [ADD] 2021-02-16 13:52:18 add leetcode_566 solution2 & test & timecost Tool

--- code/src/main/java/com/wenmq/leetcode/Solution566.java
+++ code/src/main/java/com/wenmq/leetcode/Solution566.java
@@ -23,4 +23,31 @@ public class Solution566 {
         return array;
     }
 
+    /**
+     * 方法二：
+     */
+    public int[][] matrixReshape2(int[][] nums, int r, int c) {
+        int rr = nums.length;
+        int cc = nums[0].length;
+        if (rr * cc != r * c) {
+            return nums;
+        }
+        int[][] array = new int[r][c];
+        int biasR = 0;
+        int biasC = 0;
+        for (int[] row : nums) {
+            for (int col : row) {
+                array[biasR][biasC] = col;
+                biasC++;
+                if (biasC != c) {
+                    continue;
+                }
+                biasC = 0;
+                biasR++;
+            }
+        }
+        return array;
+    }
+
+
 }
