d4494d83386418870aeaa1a89c050523a8b32c45
hellowmq <wenmq47@gmail.com>
2021-03-10 00:26:29 +0800

    [ADD] 2021-03-10 00:26:26 leetcode_392 solution & test

---
+++ code/src/main/java/com/wenmq/leetcode/Solution392.java
@@ -0,0 +1,22 @@
+package com.wenmq.leetcode;
+
+/**
+ * Q392
+ * https://leetcode-cn.com/problems/is-subsequence/
+ */
+public class Solution392 {
+    /**
+     * 方法一：
+     */
+    public boolean isSubsequence(String s, String t) {
+        int index = -1;
+        for (char c : s.toCharArray()) {
+            index = t.indexOf(c, index + 1);
+            if (index == -1) return false;
+        }
+        return true;
+    }
+
+
+
+}
