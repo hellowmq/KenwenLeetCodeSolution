200727317c3bcd7327a296696ee291ff3392c02b
hellowmq <wenmq47@gmail.com>
2021-03-10 14:35:20 +0800

    [ADD] 2021-03-10 14:35:12 leetcode_884 solution & test

---
+++ code/src/main/java/com/wenmq/leetcode/Solution884.java
@@ -0,0 +1,31 @@
+package com.wenmq.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Q884
+ * https://leetcode-cn.com/problems/maximum-gap/
+ */
+public class Solution884 {
+    /**
+     * 统计计数法，共享一个 map
+     * 如果计算 A + " " + B 会导致额外的内存空间消耗
+     */
+    public String[] uncommonFromSentences(String A, String B) {
+        Map<String, Integer> map = new HashMap<>();
+        String[] sA = A.split(" ");
+        String[] sB = B.split(" ");
+        for (String a : sA) map.put(a, map.getOrDefault(a, 0) + 1);
+        for (String a : sB) map.put(a, map.getOrDefault(a, 0) + 1);
+        ArrayList<String> list = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                list.add(entry.getKey());
+            }
+        }
+        return list.toArray(new String[0]);
+    }
+
+}
