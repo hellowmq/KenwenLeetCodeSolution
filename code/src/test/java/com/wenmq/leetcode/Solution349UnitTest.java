26a9db672fa1d641877a7a0f2cad6c9754713cee
hellowmq <wenmq47@gmail.com>
2021-03-21 21:03:18 +0800

    [ADD] 2021-03-21 21:03:09 add leetcode_349 solution and test

---
+++ code/src/test/java/com/wenmq/leetcode/Solution349UnitTest.java
@@ -0,0 +1,37 @@
+package com.wenmq.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Solution349UnitTest {
+
+    private static final String TAG = "com.wenmq.leetcode.Solution349";
+    private Solution349 mTestSolution;
+
+
+    @Before
+    public void setUp() throws Exception {
+        mTestSolution = new Solution349();
+    }
+
+    @Test
+    public void testIntersection_Case1() {
+        int[] num1 = new int[]{1, 2, 2, 1};
+        int[] num2 = new int[]{2, 2};
+        int[] expected = new int[]{2};
+        assertArrayEquals(expected, mTestSolution.intersection(num1, num2));
+        assertArrayEquals(expected, mTestSolution.intersection2(num1, num2));
+    }
+
+    @Test
+    public void testIntersection_Case2() {
+        int[] num1 = new int[]{4, 9, 5};
+        int[] num2 = new int[]{9, 4, 9, 8, 4};
+        int[] expected = new int[]{9, 4};
+        assertArrayEquals(expected, mTestSolution.intersection(num1, num2));
+        int[] expected2 = new int[]{4, 9};
+        assertArrayEquals(expected2, mTestSolution.intersection2(num1, num2));
+    }
+}
