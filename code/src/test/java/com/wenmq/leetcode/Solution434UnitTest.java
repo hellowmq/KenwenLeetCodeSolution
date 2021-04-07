1fd8564783b5d7aeba3786bb2e5c5060be60fcdc
kenwen <kenwen@tencent.com>
2021-04-07 14:00:31 +0800

    [ADD] 2021-04-07 13:59:56 leetcode434 solution and test

---
+++ code/src/test/java/com/wenmq/leetcode/Solution434UnitTest.java
@@ -0,0 +1,39 @@
+package com.wenmq.leetcode;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Solution434UnitTest {
+
+    private static final String TAG = "com.wenmq.leetcode.Solution434";
+
+    private Solution434 mTestSolution;
+
+
+    @Before
+    public void setUp() throws Exception {
+        mTestSolution = new Solution434();
+    }
+
+    @Test
+    public void testCountSegments_Case1() {
+        String str = "Hello, my name is John";
+        int expected = 5;
+        Assert.assertEquals(expected, mTestSolution.countSegments(str));
+    }
+
+    @Test
+    public void testCountSegments_Case2() {
+        String str = "                ";
+        int expected = 0;
+        Assert.assertEquals(expected, mTestSolution.countSegments(str));
+    }
+
+    @Test
+    public void testCountSegments_() {
+        String str = "    foo    bar";
+        int expected = 2;
+        Assert.assertEquals(expected, mTestSolution.countSegments(str));
+    }
+}
