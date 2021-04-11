69354fbfa5bc2cab1f371d74e0ee5f7a62ebc5ac
kenwen <kenwen@tencent.com>
2021-04-11 20:33:58 +0800

    [ADD] 2021-04-11 20:33:40 add leetcode 1773 doc

--- leetcode/src/main/java/com/wenmq/cn/leetcode/Solution1773.java
+++ leetcode/src/main/java/com/wenmq/cn/leetcode/Solution1773.java
@@ -13,6 +13,12 @@ public class Solution1773 {
      * 1 <= typei.length, colori.length, namei.length, ruleValue.length <= 10
      * ruleKey 等于 "type"、"color" 或 "name"
      * 所有字符串仅由小写字母组成
+     *
+     * NOTE：可能会发现这个算法只能超过 50% 的时间开销
+     * 使用 for i 循环可以加速
+     * 但是这受限于测试用例中 List 的使用，实际上迭代嵌套 List 似乎会在某种程度上更低效
+     * 如果是 LinkedList for i 循环就要慢得多了
+     *
      */
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
         int index = 0;
