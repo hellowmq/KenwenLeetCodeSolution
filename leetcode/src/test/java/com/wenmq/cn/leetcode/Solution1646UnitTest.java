ca40b42647d32297b4cc10a67dd6b5ba35311a4b
kenwen <kenwen@tencent.com>
2021-04-14 22:46:54 +0800

    [ADD] 2021-04-14 22:46:51 add leetcode 1646 solution and test

---
+++ leetcode/src/test/java/com/wenmq/cn/leetcode/Solution1646UnitTest.java
@@ -0,0 +1,47 @@
+package com.wenmq.cn.leetcode;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Solution1646UnitTest {
+
+    private static final String TAG = "com.wenmq.cn.leetcode.Solution1646";
+
+    private Solution1646 mTestSolution;
+
+
+    @Before
+    public void setUp() throws Exception {
+        mTestSolution = new Solution1646();
+    }
+
+    @Test
+    public void testGetMaximumGenerated_Case1() {
+        int n = 7;
+        int expected = 3;
+        Assert.assertEquals(expected, mTestSolution.getMaximumGenerated(n));
+    }
+
+    @Test
+    public void testGetMaximumGenerated_Case2() {
+        int n = 2;
+        int expected = 1;
+        Assert.assertEquals(expected, mTestSolution.getMaximumGenerated(n));
+    }
+
+    @Test
+    public void testGetMaximumGenerated_Case3() {
+        int n = 3;
+        int expected = 2;
+        Assert.assertEquals(expected, mTestSolution.getMaximumGenerated(n));
+    }
+
+    @Test
+    public void testGetMaximumGenerated_Case4() {
+        int n = 99;
+        int expected = 21;
+        Assert.assertEquals(expected, mTestSolution.getMaximumGenerated(n));
+    }
+
+}
