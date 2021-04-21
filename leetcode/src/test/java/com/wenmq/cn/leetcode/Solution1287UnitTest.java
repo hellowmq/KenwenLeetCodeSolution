c763c0d5b9e8af228cccdc3a2d7a8e90b6692012
hellowmq <wenmq47@gmail.com>
2021-04-21 11:12:46 +0800

    [ADD] 2021-04-21 11:12:40 add leetcode 1287 solution and test

--- leetcode/src/test/java/com/wenmq/cn/leetcode/Solution1287UnitTest.java
+++ leetcode/src/test/java/com/wenmq/cn/leetcode/Solution1287UnitTest.java
@@ -4,8 +4,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class Solution1287UnitTest {
 
     private static final String TAG = "com.wenmq.cn.leetcode.Solution1287";
@@ -21,7 +19,7 @@ public class Solution1287UnitTest {
     @Test
     public void testFindSpecialInteger_Case1() {
         int[] ints = {
-                1,2,2,6,6,6,6,7,10
+                1, 2, 2, 6, 6, 6, 6, 7, 10
         };
         int expected = 6;
         Assert.assertEquals(expected, mTestSolution.findSpecialInteger(ints));
