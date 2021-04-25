2c30d6667dd2103d8eecf2f2b90b1c6a4e6c6074
hellowmq <wenmq47@gmail.com>
2021-04-25 22:30:23 +0800

    [REFORMAT] 2021-04-25 22:30:08 add leetcode 783 solution and test

--- leetcode/src/main/java/com/wenmq/cn/leetcode/Solution783.java
+++ leetcode/src/main/java/com/wenmq/cn/leetcode/Solution783.java
@@ -5,25 +5,12 @@ package com.wenmq.cn.leetcode;
  * https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes/
  */
 public class Solution783 {
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
 
-//        TreeNode() {
-//        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-//        TreeNode(int val, TreeNode left, TreeNode right) {
-//            this.val = val;
-//            this.left = left;
-//            this.right = right;
-//        }
-    }
-
+    /**
+     * 树中节点数目在范围 [2, 100] 内
+     * 0 <= Node.val <= 105
+     * 差值是一个正数，其数值等于两值之差的绝对值
+     */
     public int minDiffInBST(TreeNode root) {
         return new SolutionImpl().minDiffInBST(root);
     }
@@ -51,6 +38,25 @@ public class Solution783 {
             pre = root.val;
             dfs(root.right);
         }
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+//        TreeNode() {
+//        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+//        TreeNode(int val, TreeNode left, TreeNode right) {
+//            this.val = val;
+//            this.left = left;
+//            this.right = right;
+//        }
     }
 
 }
