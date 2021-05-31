49288359e1b106d700a1808bfc617c90fa400dcc
hellowmq <wenmq47@gmail.com>
2021-05-31 20:23:50 +0800

    [ADD] 2021-05-31 20:23:38 review leetcode 1528 solution and test

--- leetcode/src/main/java/com/wenmq/cn/leetcode/Solution1528.java
+++ leetcode/src/main/java/com/wenmq/cn/leetcode/Solution1528.java
@@ -2,14 +2,14 @@ package com.wenmq.cn.leetcode;
 
 /**
  * Q1528
- * https://leetcode-cn.com/problemset/all/
+ * https://leetcode-cn.com/problems/shuffle-string/
  */
 public class Solution1528 {
     /**
      * s.length == indices.length == n
      * 1 <= n <= 100
      * s 仅包含小写英文字母。
-     * 0 <= indices[i] < n
+     * 0 <= indices[i] < n
      * indices 的所有的值都是唯一的（也就是说，indices 是整数 0 到 n - 1 形成的一组排列）。
      */
     public String restoreString(String s, int[] indices) {
