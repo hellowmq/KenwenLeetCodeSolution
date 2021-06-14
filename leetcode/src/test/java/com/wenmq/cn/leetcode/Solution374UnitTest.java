9a4af8f086351093fc57a89134618c54cbadf8d5
kenwen <kenwen@tencent.com>
2021-06-14 15:16:18 +0800

    [ADD] 2021-06-14 15:10:31 add Solution374 and Test

---
+++ leetcode/src/test/java/com/wenmq/cn/leetcode/Solution374UnitTest.java
@@ -0,0 +1,51 @@
+package com.wenmq.cn.leetcode;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Solution374UnitTest {
+
+    private Solution374.Solution mTestSolution;
+
+    @Before
+    public void setUp() throws Exception {
+        mTestSolution = new Solution374.Solution();
+    }
+
+    @Test
+    public void testGuessNumber_Case1() {
+        int n = 10;
+        int pick = 6;
+        int result = 6;
+        mTestSolution.setGuessAble(pick);
+        Assert.assertEquals(result, mTestSolution.guessNumber(n));
+    }
+
+    @Test
+    public void testGuessNumber_Case2() {
+        int n = 1;
+        int pick = 1;
+        int result = 1;
+        mTestSolution.setGuessAble(pick);
+        Assert.assertEquals(result, mTestSolution.guessNumber(n));
+    }
+
+    @Test
+    public void testGuessNumber_Case3() {
+        int n = 2;
+        int pick = 1;
+        int result = 1;
+        mTestSolution.setGuessAble(pick);
+        Assert.assertEquals(result, mTestSolution.guessNumber(n));
+    }
+
+    @Test
+    public void testGuessNumber_Case4() {
+        int n = 2;
+        int pick = 2;
+        int result = 2;
+        mTestSolution.setGuessAble(pick);
+        Assert.assertEquals(result, mTestSolution.guessNumber(n));
+    }
+}
