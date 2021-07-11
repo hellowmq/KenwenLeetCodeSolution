2efff54934d241264e966b09148625565bed4058
hellowmq <wenmq47@gmail.com>
2021-07-12 02:18:14 +0800

    [TASK] 2021-07-12 02:17:57 add shell task

---
+++ time.sh
@@ -0,0 +1,19 @@
+# Check Email
+echo "Check Email"
+EMAIL=$(git config --local user.email)
+if [[ $EMAIL == "wenmq47@gmail.com" ]]; then
+    echo "Check Email Succeed"
+else
+    echo "Check Email Failed"
+fi
+echo "------------------------------------"
+
+# Show Time
+echo "Show Time"
+date "+%Y-%m-%d %H:%M:%S"
+echo "------------------------------------"
+
+# Check Network
+echo "Check Network"
+ssh -T git@github.com
+echo "------------------------------------"
