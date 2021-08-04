53bdf0897ae8745c269b46bd5aab2073dd7fd1bf
DaftKen <wenmq47@gmail.com>
2021-08-04 17:19:02 +0800

    [FIX] 2021-08-04 17:18:51 fix doc

--- leetcode/src/main/java/com/wenmq/cn/leetcode/Solution453.java
+++ leetcode/src/main/java/com/wenmq/cn/leetcode/Solution453.java
@@ -2,7 +2,7 @@ package com.wenmq.cn.leetcode;
 
 /**
  * Q453
- * * https://leetcode-cn.com/problems/minimum-moves-to-equal-array-elements/
+ * https://leetcode-cn.com/problems/minimum-moves-to-equal-array-elements/
  */
 public class Solution453 {
     /**
