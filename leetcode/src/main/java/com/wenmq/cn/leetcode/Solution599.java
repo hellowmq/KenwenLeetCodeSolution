d9708c251717192f41323aae6b40547ddee52735
DaftKen <wenmq47@gmail.com>
2021-08-12 15:34:01 +0800

    [FIX] 2021-08-12 15:33:57 fix code review with advice

--- leetcode/src/main/java/com/wenmq/cn/leetcode/Solution599.java
+++ leetcode/src/main/java/com/wenmq/cn/leetcode/Solution599.java
@@ -7,20 +7,20 @@ import java.util.Map;
 
 /**
  * Q559
- * https://leetcode-cn.com/problemset/all/
+ * https://leetcode-cn.com/problems/minimum-index-sum-of-two-lists/
  */
 public class Solution599 {
     /**
      * 两个列表的长度范围都在 [1, 1000] 内。
      * 两个列表中的字符串的长度将在 [1，30] 的范围内。
-     * 下标从0开始，到列表的长度减1。
+     * 下标从 0 开始，到列表的长度减 1。
      * 两个列表都没有重复的元素。
      */
     public String[] findRestaurant(String[] list1, String[] list2) {
         if (list1.length > list2.length) {
             return findRestaurant(list2, list1);
         }
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>(list1.length);
         for (int i = 0; i < list1.length; i++) {
             map.put(list1[i], i);
         }
