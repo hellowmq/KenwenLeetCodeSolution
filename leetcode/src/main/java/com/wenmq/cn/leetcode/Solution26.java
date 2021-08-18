9656affa8a9feecffb2b7546e287305baf9e47e3
daftken <wenmq47@gmail.com>
2021-08-19 02:55:47 +0800

    [ADD] 2021-08-19 02:55:43 add leetcode26  solution and test

--- leetcode/src/main/java/com/wenmq/cn/leetcode/Solution26.java
+++ leetcode/src/main/java/com/wenmq/cn/leetcode/Solution26.java
@@ -2,12 +2,12 @@ package com.wenmq.cn.leetcode;
 
 /**
  * Q26
- * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/comments/
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
  */
 public class Solution26 {
     /**
-     * 0 <= nums.length <= 3 * 104
-     * -104 <= nums[i] <= 104
+     * 0 <= nums.length <= 3 * 10^4
+     * -10^4 <= nums[i] <= 10^4
      * nums 已按升序排列
      */
     public int removeDuplicates(int[] nums) {
@@ -17,15 +17,28 @@ public class Solution26 {
         int left = 0;
         for (int right = 1; right < nums.length; right++) {
             if (nums[right] != nums[left]) {
-                left++;
-                nums[left] = nums[right];
+                nums[++left] = nums[right];
             }
-
         }
         return left + 1;
-
     }
 
 
+    /**
+     * while loop
+     */
+    public int removeDuplicates2(int[] nums) {
+        int left = 0;
+        int right = 0;
+        while (right < nums.length) {
+            if (nums[right] != nums[left]) {
+                if (left++ != right) {
+                    nums[left] = nums[right];
+                }
+            }
+            right++;
+        }
+        return ++left;
+    }
 
 }
--- leetcode/src/test/java/com/wenmq/cn/leetcode/Solution26UnitTest.java
+++ leetcode/src/test/java/com/wenmq/cn/leetcode/Solution26UnitTest.java
@@ -20,34 +20,48 @@ public class Solution26UnitTest {
 
     @Test
     public void testRemoveDuplicates_Case1() {
-        int[] nums = {1, 1, 2};
-        int len = mTestSolution.removeDuplicates(nums);
-
-        int[] expected = {1, 2};
+        int[] nums = {
+                1, 1, 2
+        };
+        int[] expected = {
+                1, 2
+        };
         int expectedLen = 2;
-        Assert.assertEquals(expectedLen, len);
-        Assert.assertArrayEquals(expected, Arrays.copyOf(nums, len));
+        assertMoreSolution(nums, expected, expectedLen);
     }
 
     @Test
     public void testRemoveDuplicates_Case2() {
-        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        int len = mTestSolution.removeDuplicates(nums);
-
-        int[] expected = {0, 1, 2, 3, 4,};
+        int[] nums = {
+                0, 0, 1, 1, 1, 2, 2, 3, 3, 4
+        };
+        int[] expected = {
+                0, 1, 2, 3, 4
+        };
         int expectedLen = 5;
-        Assert.assertEquals(expectedLen, len);
-        Assert.assertArrayEquals(expected, Arrays.copyOf(nums, len));
+        assertMoreSolution(nums, expected, expectedLen);
     }
 
     @Test
     public void testRemoveDuplicates_Case3() {
-        int[] nums = {};
-        int len = mTestSolution.removeDuplicates(nums);
-
-        int[] expected = {};
+        int[] nums = {
+        };
+        int[] expected = {
+        };
         int expectedLen = 0;
-        Assert.assertEquals(expectedLen, len);
-        Assert.assertArrayEquals(expected, Arrays.copyOf(nums, len));
+        assertMoreSolution(nums, expected, expectedLen);
+    }
+
+
+    private void assertMoreSolution(int[] nums, int[] expected, int expectedLen) {
+        int[] sourceArray1 = Arrays.copyOf(nums, nums.length);
+        int actual = mTestSolution.removeDuplicates(sourceArray1);
+        Assert.assertEquals(expectedLen, actual);
+        Assert.assertArrayEquals(expected, Arrays.copyOf(sourceArray1, actual));
+        // solution2
+        int[] sourceArray2 = Arrays.copyOf(nums, nums.length);
+        int actual2 = mTestSolution.removeDuplicates2(sourceArray2);
+        Assert.assertEquals(expectedLen, actual2);
+        Assert.assertArrayEquals(expected, Arrays.copyOf(sourceArray2, actual2));
     }
 }
