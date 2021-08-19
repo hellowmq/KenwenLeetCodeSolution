b682091f082b242f6efc708a613509be29a83ab6
daftken <wenmq47@gmail.com>
2021-08-20 04:34:15 +0800

    [ADD] 2021-08-20 04:09:38 rename leetcode 304 solution & test

--- leetcode/src/main/java/com/wenmq/cn/leetcode/Solution304.java
+++ leetcode/src/main/java/com/wenmq/cn/leetcode/Solution304.java
@@ -1,14 +1,24 @@
 package com.wenmq.cn.leetcode;
 
 /**
+ * <a href="https://leetcode-cn.com/problems/range-sum-query-2d-immutable/">
  * Q304
- * https://leetcode-cn.com/problems/range-sum-query-2d-immutable/
+ * </a>
  */
 public class Solution304 {
 
+    /**
+     * m == matrix.length
+     * n == matrix[i].length
+     * 1 <= m, n <= 200
+     * -10^5 <= matrix[i][j] <= 10^5
+     * 0 <= row1 <= row2 < m
+     * 0 <= col1 <= col2 < n
+     * 最多调用 10^4 次 sumRegion 方法
+     */
     public static class NumMatrix implements Solution {
 
-        int[][] array;
+        int[][] mMatrix;
 
         public NumMatrix(int[][] matrix) {
             if (matrix.length == 0 || matrix[0].length == 0) {
@@ -16,25 +26,26 @@ public class Solution304 {
             }
             int m = matrix.length + 1;
             int n = matrix[0].length + 1;
-            array = new int[m][n];
+            mMatrix = new int[m][n];
             for (int i = 0; i < matrix.length; i++) {
                 for (int j = 0; j < matrix[i].length; j++) {
-                    array[i + 1][j + 1] = matrix[i][j] + array[i][j + 1] + array[i + 1][j] - array[i][j];
+                    mMatrix[i + 1][j + 1] =
+                            matrix[i][j] + mMatrix[i][j + 1] + mMatrix[i + 1][j] - mMatrix[i][j];
                 }
             }
         }
 
         @Override
         public int sumRegion(int row1, int col1, int row2, int col2) {
-            return array[row2 + 1][col2 + 1] + array[row1][col1]
-                    - array[row2 + 1][col1] - array[row1][col2 + 1];
+            return mMatrix[row2 + 1][col2 + 1] + mMatrix[row1][col1]
+                    - mMatrix[row2 + 1][col1] - mMatrix[row1][col2 + 1];
         }
     }
 
 
     public static class NumMatrix2 implements Solution {
 
-        int[][] array;
+        int[][] mMatrix;
 
         public NumMatrix2(int[][] matrix) {
             int m = matrix.length + 1;
@@ -42,20 +53,20 @@ public class Solution304 {
             if (m > 1) {
                 n = matrix[0].length + 1;
             }
-            array = new int[m][n];
+            mMatrix = new int[m][n];
             for (int i = 0; i < matrix.length; i++) {
                 int sum = 0;
                 for (int j = 0; j < matrix[i].length; j++) {
                     sum += matrix[i][j];
-                    array[i + 1][j + 1] = array[i][j + 1] + sum;
+                    mMatrix[i + 1][j + 1] = mMatrix[i][j + 1] + sum;
                 }
             }
         }
 
         @Override
         public int sumRegion(int row1, int col1, int row2, int col2) {
-            return array[row2 + 1][col2 + 1] + array[row1][col1]
-                    - array[row2 + 1][col1] - array[row1][col2 + 1];
+            return mMatrix[row2 + 1][col2 + 1] + mMatrix[row1][col1]
+                    - mMatrix[row2 + 1][col1] - mMatrix[row1][col2 + 1];
         }
     }
 
