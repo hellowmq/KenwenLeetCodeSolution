cea8a5b1b9914402fedd73d0502d826bf6c2dd4f
daftken <wenmq47@gmail.com>
2021-08-20 04:15:46 +0800

    [ADD] 2021-08-20 04:09:38 rename leetcode 303 solution & test

--- leetcode/src/test/java/com/wenmq/cn/leetcode/Solution303UnitTest.java
+++ leetcode/src/test/java/com/wenmq/cn/leetcode/Solution303UnitTest.java
@@ -2,6 +2,8 @@ package com.wenmq.cn.leetcode;
 
 import static com.wenmq.cn.leetcode.Solution303.NumArray;
 
+import com.wenmq.cn.leetcode.Solution303.NumArray2;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,7 +13,6 @@ public class Solution303UnitTest {
     private static final String TAG = "com.wenmq.cn.leetcode.Solution303";
     private Solution303 mTestSolution;
 
-
     @Before
     public void setUp() throws Exception {
         mTestSolution = new Solution303();
@@ -19,7 +20,9 @@ public class Solution303UnitTest {
 
     @Test
     public void test_NumArrayCase0() {
-        String[] commands = new String[]{"NumArray", "sumRange", "sumRange", "sumRange"};
+        String[] commands = new String[]{
+                "NumArray", "sumRange", "sumRange", "sumRange"
+        };
         int[][] parameters = new int[][]{
                 {-2, 0, 3, -5, 2, -1},
                 {0, 2},
@@ -46,9 +49,32 @@ public class Solution303UnitTest {
     }
 
     @Test
-    public void test_NumArrayCaseNull() {
-        NumArray numArray = new NumArray(new int[0]);
-        Assert.assertEquals(0, numArray.nums.length);
-        Assert.assertEquals(0, numArray.res.length);
+    public void test_NumArray2Case0() {
+        String[] commands = new String[]{
+                "NumArray", "sumRange", "sumRange", "sumRange"
+        };
+        int[][] parameters = new int[][]{
+                {-2, 0, 3, -5, 2, -1},
+                {0, 2},
+                {2, 5},
+                {0, 5},
+        };
+        Integer[] result = new Integer[]{
+                null, 1, -1, -3
+        };
+        NumArray2 numArray = null;
+        for (int i = 0; i < commands.length; i++) {
+            switch (commands[i]) {
+                case "NumArray":
+                    numArray = new NumArray2(parameters[i]);
+                    break;
+                case "sumRange":
+                    int actual = numArray.sumRange(parameters[i][0], parameters[i][1]);
+                    Assert.assertEquals(result[i], Integer.valueOf(actual));
+                    break;
+                default:
+                    // ignore
+            }
+        }
     }
 }
