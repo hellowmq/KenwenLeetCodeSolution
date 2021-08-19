15e571544b10d2d754409d9a034eda2be998291e
daftken <wenmq47@gmail.com>
2021-08-20 04:15:46 +0800

    [ADD] 2021-08-20 04:09:38 rename leetcode 303 solution & test

--- leetcode/src/main/java/com/wenmq/cn/leetcode/Solution303.java
+++ leetcode/src/main/java/com/wenmq/cn/leetcode/Solution303.java
@@ -1,8 +1,9 @@
 package com.wenmq.cn.leetcode;
 
 /**
+ * <a href="https://leetcode-cn.com/problems/range-sum-query-immutable/">
  * Q303
- * https://leetcode-cn.com/problems/range-sum-query-immutable/
+ * </a>
  */
 public class Solution303 {
     /**
@@ -10,30 +11,44 @@ public class Solution303 {
      * -10^5<= nums[i] <= 10^5
      * 0 <= i <= j < nums.length
      * 最多调用 10^4 次 sumRange 方法
-     *
      */
     static class NumArray {
-        public int[] nums;
-        public int[] res;
+        public int[] mNums;
+        public int[] mRes;
 
         public NumArray(int[] nums) {
-            this.nums = nums;
-            this.res = new int[nums.length];
-            if (nums.length == 0) {
-                return;
-            }
-            res[0] = nums[0];
-            for (int i = 1; i < res.length; i++) {
-                res[i] = res[i - 1] + nums[i];
-
+            this.mNums = nums;
+            this.mRes = new int[nums.length];
+            if (nums.length != 0) {
+                mRes[0] = nums[0];
+                for (int i = 1; i < mRes.length; i++) {
+                    mRes[i] = mRes[i - 1] + nums[i];
+                }
             }
         }
 
         public int sumRange(int i, int j) {
-            if (i == 0) {
-                return res[j];
+            return i == 0 ? mRes[j] : mRes[j] - mRes[i - 1];
+        }
+    }
+
+    /**
+     * 优化方法复杂度
+     */
+    static class NumArray2 {
+        public int[] mNums;
+        public int[] mRes;
+
+        public NumArray2(int[] nums) {
+            this.mNums = nums;
+            this.mRes = new int[nums.length + 1];
+            for (int i = 0; i < nums.length; i++) {
+                mRes[i + 1] = mRes[i] + nums[i];
             }
-            return res[j] - res[i - 1];
+        }
+
+        public int sumRange(int i, int j) {
+            return mRes[j + 1] - mRes[i];
         }
     }
 
