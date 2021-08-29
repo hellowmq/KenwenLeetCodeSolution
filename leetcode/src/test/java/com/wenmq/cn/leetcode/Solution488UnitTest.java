17966abead1353ee6b754927755dcfe620f37dae
DaftKen <wenmq47@gmail.com>
2021-08-29 18:05:51 +0800

    [ADD] 2021-08-29 18:05:48 add leetcode Solution488 and test

--- leetcode/src/test/java/com/wenmq/cn/leetcode/Solution488UnitTest.java
+++ leetcode/src/test/java/com/wenmq/cn/leetcode/Solution488UnitTest.java
@@ -17,10 +17,13 @@ public class Solution488UnitTest {
 
     @Test
     public void testNextGreaterElements_Case1() {
-        int[] array = new int[]{1, 2, 1};
-        int[] actualArray = mSolution488.nextGreaterElements(array);
-        int[] expectArray = new int[]{2, -1, 2};
-        Assert.assertArrayEquals(expectArray, actualArray);
-
+        int[] array = new int[]{
+                1, 2, 1
+        };
+        int[] expectArray = new int[]{
+                2, -1, 2
+        };
+        Assert.assertArrayEquals(expectArray, mSolution488.nextGreaterElements(array));
+        Assert.assertArrayEquals(expectArray, mSolution488.nextGreaterElements2(array));
     }
 }
