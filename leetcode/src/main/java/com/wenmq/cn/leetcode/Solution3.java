b5fd47370335377f18ff32c41f2c48d47b7f65fc
DaftKen <wenmq47@gmail.com>
2021-09-09 12:19:38 +0800

    [ADD] 2021-08-26 13:19:09 add leetcode Solution3 and test
    * [ADD] 2021-08-26 13:19:09 add leetcode Solution1913 and test
    
    * [ADD] 2021-08-20 04:03:59 fix code review problem.

---
+++ leetcode/src/main/java/com/wenmq/cn/leetcode/Solution3.java
@@ -0,0 +1,31 @@
+package com.wenmq.cn.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * <a href="https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/">
+ * Q3
+ * </a>
+ */
+public class Solution3 {
+    /**
+     * 0 <= s.length <= 5 * 10^4
+     * s 由英文字母、数字、符号和空格组成
+     */
+    public int lengthOfLongestSubstring(String s) {
+        // 记录字符上一次出现的位置
+        int[] last = new int[128];
+        Arrays.fill(last, -1);
+        int n = s.length();
+        int res = 0;
+        int start = 0; // 窗口开始位置
+        for (int i = 0; i < n; i++) {
+            char index = s.charAt(i);
+            start = Math.max(start, last[index] + 1);
+            res = Math.max(res, i - start + 1);
+            last[index] = i;
+        }
+        return res;
+    }
+
+}
